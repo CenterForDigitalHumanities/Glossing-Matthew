298ed8d6fd2dff8d0be902221bf3d441c751d73d
Patrick Cuba <cubap@slu.edu>
2021-07-30 14:04:34 -0500

    Data to prod (#12)
    * Point to Tiny Matt on Dev.  We can test what it is like to move data from one collection to another through DEER's natural functionality.  Once we confirm the Collection and Annotations on its members will move as expected, we can do this same thing on prod with a Tiny Matt Test on Prod to make sure the 'import' works via an update across origin API instances.
    
    * synching
    
    * short circuit matching for now
    
    * prepping for data migration
    
    * deletes on prod
    
    * updated for production
    
    Co-authored-by: Bryan Haberberger <bryan.j.haberberger@slu.edu>

--- js/deer-render.js
+++ js/deer-render.js
@@ -265,8 +265,8 @@ DEER.TEMPLATES.lines = function (obj, options = {}) {
             function selectLine(event) {
                 const lastClick = document.querySelector("line.just")
                 const line = event.target.closest("line")
-                const SHIFT = event.shiftKey
-                if (lastClick && SHIFT) {
+
+                if (lastClick && event.shiftKey) {
                     // band-select
                     const change = lastClick.classList.contains("selected") // change is constant
                         ? "add"
@@ -286,11 +286,14 @@ DEER.TEMPLATES.lines = function (obj, options = {}) {
                         line.classList.toggle("selected")
                     }
                 }
+
                 if (lastClick) { lastClick.classList.remove("just") }
+
                 if(!line.classList.contains("located")){
                     line.classList.add("just")
                 }
             }
+
             const controls = elem.querySelectorAll("a.tag:not(.gloss-location)")
             for (const b of controls) {
                 b.addEventListener("click",e=>{
