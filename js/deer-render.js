2f7c92d6ae106d96ab2c2ae4b8972685d0afe568
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-05-19 12:04:21 -0500

    HTTPS link updates and httpsIdArray logic for querying (#51)
    * HTTPS link updates and httpsIdArray logic for querying
    
    * off fix the header on index
    
    * A better index and header/footer consistency
    
    * Minimal footer for index
    
    * https T-PEN links and a couple text changes.

--- js/deer-render.js
+++ js/deer-render.js
@@ -173,7 +173,7 @@ DEER.TEMPLATES.thumbs = function (obj, options = {}) {
     return {
         html: obj["tpen://base-project"] ? `<div class="is-full-width"> <h3> ... loading images ... </h3> </div>` : ``,
         then: (elem) => {
-            fetch("//t-pen.org/TPEN/manifest/" + obj["tpen://base-project"].value)
+            fetch("https://t-pen.org/TPEN/manifest/" + obj["tpen://base-project"].value)
                 .then(response => response.json())
                 .then(ms => elem.innerHTML = `
                 ${ms.sequences[0].canvases.slice(0, 10).reduce((a, b) => a += `<img class="thumbnail" src="${b.images[0].resource['@id']}">`, ``)}
@@ -191,7 +191,7 @@ DEER.TEMPLATES.folioTranscription = function (obj, options = {}) {
         html: obj.tpenProject ? `<div class="is-full-width"> <h3> ... loading preview ... </h3> </div>` : ``,
         then: (elem) => {
             const url = obj.tpenProject[0]?.value ?? obj.tpenProject.value
-            fetch("//t-pen.org/TPEN/manifest/" + url)
+            fetch("https://t-pen.org/TPEN/manifest/" + url)
                 .then(response => response.json())
                 .then(ms => elem.innerHTML = `
                 ${ms.sequences[0].canvases.slice(0, 10).reduce((a, b) => a += `
@@ -421,7 +421,7 @@ DEER.TEMPLATES.glossLines = function (obj, options = {}) {
                 const historyWildcard = { $exists: true, $type: 'array', $eq: [] }
                 const query = {
                     motivation: "linking",
-                    'partOf.id': location.hash.substr(1),
+                    'partOf.id': httpsIdArray(location.hash.substr(1)),
                     '__rerum.history.next': historyWildcard
                 }
 
@@ -703,7 +703,7 @@ DEER.TEMPLATES.lines_new = function (obj, options = {}) {
                 const historyWildcard = { $exists: true, $type: 'array', $eq: [] }
 
                 const query = {
-                    forCanvas: c['@id'],
+                    forCanvas: httpsIdArray(c['@id']),
                     motivation: "classifying",
                     'body.location': { $exists: true },
                     '__rerum.history.next': historyWildcard
@@ -984,7 +984,7 @@ DEER.TEMPLATES.lines = function (obj, options = {}) {
                 const historyWildcard = { $exists: true, $type: 'array', $eq: [] }
 
                 const query = {
-                    target: c['@id'],
+                    target: httpsIdArray(['@id']),
                     motivation: "classifying",
                     'body.locations': { $exists: true },
                     '__rerum.history.next': historyWildcard
@@ -1152,17 +1152,17 @@ DEER.TEMPLATES.managedlist = function (obj, options = {}) {
                             }, {
                                 "body.targetCollection": collection
                             }],
-                            target: id,
+                            target: httpsIdArray(id),
                             "__rerum.history.next": historyWildcard
                         }
-                        fetch("//tinymatt.rerum.io/gloss/query", {
+                        fetch("https://tinymatt.rerum.io/gloss/query", {
                             method: "POST",
                             body: JSON.stringify(queryObj)
                         })
                             .then(r => r.ok ? r.json() : Promise.reject(new Error(r?.text)))
                             .then(annos => {
                                 let all = annos.map(anno => {
-                                    return fetch("//tinymatt.rerum.io/gloss/delete", {
+                                    return fetch("https://tinymatt.rerum.io/gloss/delete", {
                                         method: "DELETE",
                                         body: anno["@id"],
                                         headers: {
@@ -1282,7 +1282,7 @@ DEER.TEMPLATES.person = function (obj, options = {}) {
 DEER.TEMPLATES.pageRanges = function (obj, options = {}) {
     return {
         then: (elem) => {
-            let queryObj = { "body.isPartOf.value": obj['@id'] }
+            let queryObj = { "body.isPartOf.value": httpsIdArray(obj['@id']) }
             fetch(DEER.URLS.QUERY, {
                 method: "POST",
                 mode: "cors",
@@ -1474,4 +1474,10 @@ export function initializeDeerViews(config) {
         //Failed 5 times at 100
         //Failed 0 times at 200
     })
+}
+
+function httpsIdArray(id,justArray) {
+    if (!id.startsWith("http")) return justArray ? [ id ] : id
+    if (id.startsWith("https://")) return justArray ? [ id, id.replace('https','http') ] : { $in: [ id, id.replace('https','http') ] }
+    return justArray ? [ id, id.replace('http','https') ] : { $in: [ id, id.replace('http','https') ] }
 }
